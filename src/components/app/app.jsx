fea1b9e67cf34c9ca36a02c4b8fef5506613cc7e
de-evg <d.e.min@yandex.ru>
2020-10-02 09:29:24 +0300

    installed react-dom-router

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
 import WelcomeScreen from "../welcome-screen/welcome-screen";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 
 const App = (props) => {
   const {errorsCount} = props;
 
   return (
-    <WelcomeScreen errorsCount={errorsCount} />
+    <BrowserRouter>
+      <Route exact path="/">
+        <WelcomeScreen errorsCount={errorsCount} />
+      </Route>
+      <Route exact path="/dev-artist">
+      </Route>
+      <Route exact path="/dev-genre">
+      </Route>
+      <Route exact path="/login">
+      </Route>
+      <Route exact path="/result">
+      </Route>
+      <Route exact path="/lose">
+      </Route>
+    </BrowserRouter>
+    
   );
 };
 
