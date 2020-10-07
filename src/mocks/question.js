2e996980e25b160174c617884fe5273d3618331d
de-evg <d.e.min@yandex.ru>
2020-10-07 11:41:30 +0300

    update mocks

--- src/mocks/question.js
+++ src/mocks/question.js
@@ -1,3 +1,5 @@
+const AVATAR_URL = `https://api.adorable.io/avatars/128`;
+
 const questions = [
   {
     type: `genre`,
@@ -16,6 +18,24 @@ const questions = [
       {
         src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
         genre: `rock`
+      }
+    ]
+  }, {
+    type: `artist`,
+    song: {
+      artist: `Jim Beam`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`
+    },
+    answers: [
+      {
+        picture: `${AVATAR_URL}/${Math.random()}`,
+        artist: `John Snow`
+      }, {
+        picture: `${AVATAR_URL}/${Math.random()}`,
+        artist: `Jack Daniels`
+      }, {
+        picture: `${AVATAR_URL}/${Math.random()}`,
+        artist: `Jim Beam`
       }
     ]
   }
