d5161f2c66b401a9f49f8acf751bff940ea9107e
de-evg <d.e.min@yandex.ru>
2020-10-30 10:49:19 +0300

    added redux-thunk in index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from "react";
 import {render} from "react-dom";
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
 import {Provider} from "react-redux";
+import thunk from "redux-think";
+import {createAPI} from "./services/api";
 import App from "./components/app/app";
 import {reducer} from "./store/reducer";
+import {requireAuthorization} from "./store/action";
+import {fetchQuestionList, checkAuth} from "./store/api-actions";
+import {AuthorizationStatus} from "./const";
 
+const api = createAPI(
+    () => store.dispatch(requireAuthorization(AuthorizationStatus.NO_AUTH))
+);
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+    applyMiddleware(thunk.withExtraArgument(api))
 );
+
+store.dispatch(fetch(fetchQuestionList()));
+store.dispatch(checkAuth());
 
 render(
     <Provider store={store}>
